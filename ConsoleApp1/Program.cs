790d884287b5b4f5776f6fdb5360d759655edb11
Ethan Behar <behar.ethan@gmail.com>
2020-03-11 07:48:53 -0400

    count letters linq solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,15 +12,20 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Count Letter Test");
             Dictionary<char, int> test = Utility.CountLettersOriginal("Hey, this is my first test.");
+            Console.WriteLine("Original Solution: ");
+            foreach (KeyValuePair<char, int> pair in test)
+                Console.WriteLine("{0} : {1}", pair.Key, pair.Value);
 
+            test = Utility.CountLettersLinq("Hey, this is my first test.");
+            Console.WriteLine("LINQ Solution: ");
             foreach (KeyValuePair<char, int> pair in test)
                 Console.WriteLine("{0} : {1}", pair.Key, pair.Value);
 
             Console.WriteLine();
 
             Console.WriteLine("Reverse String Test");
-            Console.WriteLine(Utility.ReverseStringOriginal("Hey, this is my first test."));
-            Console.WriteLine(Utility.ReverseStringLinqSolution("Hey, this is my first test."));
+            Console.WriteLine("Original Solution: " + Utility.ReverseStringOriginal("Hey, this is my first test."));
+            Console.WriteLine("LINQ Solution: " + Utility.ReverseStringLinqSolution("Hey, this is my first test."));
 
 
             Console.WriteLine();
--- ConsoleApp1/Utility.cs
+++ ConsoleApp1/Utility.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp1
 
         public static Dictionary<char, int> CountLettersOriginal(string input)
         {
-
             Dictionary<char, int> toBeReturned = new Dictionary<char, int>();
 
             var mutatedInput = Regex.Replace(input.ToLower(), @"[^0-9a-zA-Z]+", "");
@@ -27,6 +26,19 @@ namespace ConsoleApp1
             return toBeReturned;
         }
 
+        public static Dictionary<char, int> CountLettersLinq(string input)
+        {
+            var mutatedInput = Regex.Replace(input.ToLower(), @"[^0-9a-zA-Z]+", "");
+
+            var query = mutatedInput
+                .GroupBy(c => c)
+                .Select(c => new { Char = c.Key, Count = c.Count() });
+
+            var toBeReturned = query.ToDictionary(q => q.Char, q => q.Count);
+
+            return toBeReturned;
+        }
+
         public static string ReverseStringOriginal(string input)
         {
             List<char> reverseMe = new List<char>(input.ToCharArray());
@@ -37,9 +49,6 @@ namespace ConsoleApp1
             return toBeReturn;
         }
 
-        // Interestingly I had to google this one but no ReverseWordsLinqSolution
-        // It's funny how I use both Reverse() and ToArray()
-        // In the original solution but add extra steps to complete the task
         public static string ReverseStringLinqSolution(string input)
         {
             return new string(input.Reverse().ToArray());
@@ -63,16 +72,11 @@ namespace ConsoleApp1
 
         public static string ReverseWordsLinqSolution(string input)
         {
-            // Use a builder to return value
             StringBuilder returnMe = new StringBuilder();
 
-            // Split initial string to string[] so we can ForEach
             var reverseMe = input
                 .Split(' ');
 
-            // Iterate each string, 
-            // Reverse it and convert away from IEnumerable back to a string 
-            // Append to string builder
             reverseMe
                 .ToList()
                 .ForEach(s => returnMe.Append(new string(s.Reverse().ToArray()) + " "));
